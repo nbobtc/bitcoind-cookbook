dc043f3ac9180e237dbecca6b32688e7670b7437
Joshua Estes <Joshua@dSpaceLabs.com>
2014-12-12 07:25:03 -0500

    A few bug fixes and a few more features, next I need to document everything

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,39 +3,36 @@
 # Default attributes
 #
 
-#BITCOIND_BIN="/usr/bin/bitcoind"
-#BITCOIND_USER="bitcoin"
-#BITCOIND_GROUP="bitcoin"
-#BITCOIND_PIDDIR="/var/run/bitcoind"
-#BITCOIND_PIDFILE="/var/run/bitcoind/bitcoind.pid"
-#BITCOIND_CONFIGFILE="/etc/bitcoin/bitcoin.conf"
-#BITCOIND_DATADIR="/var/lib/bitcoind"
-
 # Install via `package` (recommended) or by `source`
 default['bitcoind']['install_via'] = 'package'
 
-# User/Group to run bitcoin as
-default['bitcoind']['user']  = 'bitcoin'
-default['bitcoind']['group'] = 'bitcoin'
+default['bitcoind']['bin']        = '/usr/bin/bitcoind'
+default['bitcoind']['user']       = 'bitcoin'
+default['bitcoind']['group']      = 'bitcoin'
+default['bitcoind']['piddir']     = '/var/run/bitcoind'
+default['bitcoind']['pidfile']    = '/var/run/bitcoind/bitcoind.pid'
+default['bitcoind']['configfile'] = '/etc/bitcoin/bitcoin.conf'
+default['bitcoind']['datadir']    = '/var/lib/bitcoind'
+
+# Used in the init script
+default['bitcoind']['wallet']['enabled'] = true
+
+# Template to use
+default['bitcoind']['conf']['template'] = 'bitcoin.conf.erb'
+default['bitcoind']['conf']['options']  = {
+  'datadir'     => node['bitcoind']['datadir'],
+  '#testnet'    => '1',
+  'server'      => '1',
+  '#rpcssl'      => '1',
+  '#rpcport'     => '8333',
+  'rpcuser'     => 'changeme',
+  'rpcpassword' => 'YourSuperGreatPasswordNumber_DO_NOT_USE_THIS_OR_YOU_WILL_GET_ROBBED_385593',
+}
 
 # Packages needed to be installed to compile
 default['bitcoind']['packages'] = %w{ git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev }
 # Configuration flags
 default['bitcoind']['source']['configure_flags'] = '--without-miniupnpc --enable-harding'
-
-# Where to keep the data files
-default['bitcoind']['datadir'] = '/etc/bitcoin'
-
-# Template to use
-default['bitcoind']['conf']['template'] = 'bitcoin.conf.erb'
-default['bitcoind']['conf']['options'] = {
-  '#testnet'    => '1',
-  'server'      => '1',
-  'rpcssl'      => '1',
-  'rpcport'     => '8333',
-  'rpcuser'     => 'changeme',
-  'rpcpassword' => 'YourSuperGreatPasswordNumber_DO_NOT_USE_THIS_OR_YOU_WILL_GET_ROBBED_385593',
-}
 
 # repository that is cloned to be compiled
 default['bitcoind']['git']['repo'] = 'https://github.com/bitcoin/bitcoin.git'
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,28 +10,55 @@ include_recipe "bitcoind::#{node['bitcoind']['install_via']}"
 # Create bitcoind user and group
 user node['bitcoind']['user'] do
   comment 'bitcoind system user'
-  #gid node['bitcoind']['group']
   system true
   shell '/bin/false'
 end
 
-template "/etc/init/bitcoind.conf" do
+# /var/lib/bitcoind
+directory node['bitcoind']['datadir'] do
+  owner node['bitcoind']['user']
+  group node['bitcoind']['group']
+end
+
+# /etc/bitcoin
+directory File.dirname(node['bitcoind']['configfile']) do
+  owner node['bitcoind']['user']
+  group node['bitcoind']['group']
+end
+
+template '/etc/init/bitcoind.conf' do
   source 'init/bitcoind.conf.erb'
   owner node['bitcoind']['user']
   group node['bitcoind']['group']
   mode 0600
+  variables(
+    :bin            => node['bitcoind']['bin'],
+    :user           => node['bitcoind']['user'],
+    :group          => node['bitcoind']['group'],
+    :piddir         => node['bitcoind']['piddir'],
+    :pidfile        => node['bitcoind']['pidfile'],
+    :configfile     => node['bitcoind']['configfile'],
+    :datadir        => node['bitcoind']['datadir'],
+    :wallet_enabled => node['bitcoind']['wallet']['enabled'],
+  )
+  notifies :restart, 'service[bitcoind]', :immediately
 end
 
-####
-#
-# /etc/bitcoin/bitcoin.conf file
-#
-directory "#{node['bitcoind']['datadir']}" do
-end
-template "#{node['bitcoind']['datadir']}/bitcoin.conf" do
+# /etc/bitcoin/bitcoin.conf
+template node['bitcoind']['configfile'] do
   source node['bitcoind']['conf']['template']
   owner node['bitcoind']['user']
   group node['bitcoind']['group']
   mode 0600
-  variables(:options => node['bitcoind']['conf']['options'])
+  variables(
+    :options => node['bitcoind']['conf']['options'],
+  )
+  notifies :restart, 'service[bitcoind]', :immediately
+end
+
+# bitcoind service
+service 'bitcoind' do
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
 end
